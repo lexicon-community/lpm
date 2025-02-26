7cc223d0419a6c5997f2faa4e65b61d5ad311b1f
Tom Sherman <the.tomsherman@gmail.com>
2025-02-26 17:58:47 +0000

    Remove mock

--- bin.ts
+++ bin.ts
@@ -1,41 +1,4 @@
-import { AtUri } from "@atproto/syntax";
-import { NSID } from "@atproto/syntax";
 import { Commands } from "./commands.ts";
 import { globalContainer } from "./container.ts";
-import { NodeRegistry } from "./node-registry.ts";
-import { Node } from "./node.ts";
-
-// Temporarily mocking registry because strongRef isn't published yet
-class MockedNodeRegistry extends NodeRegistry {
-  override get(nsid: NSID): Node {
-    if (
-      ["com.atproto.repo.strongRef", "com.atproto.label.defs"].includes(
-        nsid.toString()
-      )
-    ) {
-      return {
-        nsid,
-        // @ts-expect-error
-        async resolve() {
-          return {
-            nsid,
-            children: [],
-            doc: { defs: [] },
-            success: true,
-            uri: new AtUri(
-              `at://did:plc:6msi3pj7krzih5qxqtryxlzw/com.atproto.lexicon.schema/${nsid.toString()}`
-            ),
-          };
-        },
-      };
-    }
-
-    return super.get(nsid);
-  }
-}
-globalContainer.bind({
-  provide: NodeRegistry,
-  useClass: MockedNodeRegistry,
-});
 
 await globalContainer.get(Commands).fetch();
--- node.test.ts
+++ node.test.ts
@@ -1,42 +1,8 @@
-import { AtUri, NSID } from "@atproto/syntax";
+import { NSID } from "@atproto/syntax";
 import { globalContainer } from "./container.ts";
-import { Node, Resolution } from "./node.ts";
+import { Resolution } from "./node.ts";
 import { NodeRegistry } from "./node-registry.ts";
 import { assertEquals, assertObjectMatch } from "jsr:@std/assert";
-
-const container = globalContainer.inner.createChild();
-
-// Temporarily mocking registry because strongRef isn't published yet
-class MockedNodeRegistry extends NodeRegistry {
-  override get(nsid: NSID): Node {
-    if (
-      ["com.atproto.repo.strongRef", "com.atproto.label.defs"].includes(
-        nsid.toString()
-      )
-    ) {
-      return {
-        nsid,
-        // @ts-expect-error
-        async resolve() {
-          return {
-            children: [],
-            doc: { defs: [] },
-            success: true,
-            uri: new AtUri(
-              `at://did:plc:6msi3pj7krzih5qxqtryxlzw/com.atproto.lexicon.schema/${nsid.toString()}`
-            ),
-          };
-        },
-      };
-    }
-
-    return super.get(nsid);
-  }
-}
-container.bind({
-  provide: NodeRegistry,
-  useClass: MockedNodeRegistry,
-});
 
 function assertSuccessfullResolution(
   data: Resolution,
@@ -46,7 +12,7 @@ function assertSuccessfullResolution(
 }
 
 Deno.test("resolves uri", async () => {
-  const registry = container.get(NodeRegistry);
+  const registry = globalContainer.get(NodeRegistry);
 
   const resolution = await registry
     .get(NSID.parse("com.atproto.lexicon.schema"))
@@ -60,7 +26,7 @@ Deno.test("resolves uri", async () => {
 });
 
 Deno.test("node children", async () => {
-  const registry = container.get(NodeRegistry);
+  const registry = globalContainer.get(NodeRegistry);
 
   const resolution = await registry
     .get(NSID.parse("app.bsky.actor.profile"))
@@ -76,7 +42,7 @@ Deno.test("node children", async () => {
 });
 
 Deno.test.only("registry resolve", async () => {
-  const registry = container.get(NodeRegistry);
+  const registry = globalContainer.get(NodeRegistry);
 
   const uris = [];
 
@@ -88,10 +54,13 @@ Deno.test.only("registry resolve", async () => {
     console.log(node.uri.toString());
   }
 
-  // assertEquals(uris, [
-  //   "at://did:plc:4v4y5r3lwsbtmsxhile2ljac/com.atproto.lexicon.schema/app.bsky.graph.getList",
-  //   "at://did:plc:4v4y5r3lwsbtmsxhile2ljac/com.atproto.lexicon.schema/app.bsky.graph.defs",
-  //   "at://did:plc:4v4y5r3lwsbtmsxhile2ljac/com.atproto.lexicon.schema/app.bsky.actor.defs",
-  //   "at://did:plc:6msi3pj7krzih5qxqtryxlzw/com.atproto.lexicon.schema/com.atproto.repo.strongRef",
-  // ]);
+  assertEquals(uris, [
+    "at://did:plc:4v4y5r3lwsbtmsxhile2ljac/com.atproto.lexicon.schema/app.bsky.feed.getPosts",
+    "at://did:plc:4v4y5r3lwsbtmsxhile2ljac/com.atproto.lexicon.schema/app.bsky.feed.defs",
+    "at://did:plc:4v4y5r3lwsbtmsxhile2ljac/com.atproto.lexicon.schema/app.bsky.actor.defs",
+    "at://did:plc:6msi3pj7krzih5qxqtryxlzw/com.atproto.lexicon.schema/com.atproto.label.defs",
+    "at://did:plc:4v4y5r3lwsbtmsxhile2ljac/com.atproto.lexicon.schema/app.bsky.richtext.facet",
+    "at://did:plc:4v4y5r3lwsbtmsxhile2ljac/com.atproto.lexicon.schema/app.bsky.graph.defs",
+    "at://did:plc:6msi3pj7krzih5qxqtryxlzw/com.atproto.lexicon.schema/com.atproto.repo.strongRef",
+  ]);
 });
