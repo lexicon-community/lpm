cd8d1fc0fc6ea70bd2796d5bce7209d64494e425
Tom Sherman <tom@sherman.is>
2025-03-07 11:53:14 +0000

    WIP validate post record

--- packages/core/src/node.test.ts
+++ packages/core/src/node.test.ts
@@ -4,6 +4,7 @@ import { NodeRegistry } from "./node-registry.ts";
 import { assertEquals, assertObjectMatch } from "jsr:@std/assert";
 import { bootstrap } from "@needle-di/core";
 import { Lexicons } from "@atproto/lexicon";
+import { skip } from "node:test";
 
 function assertSuccessfullResolution(
   data: Resolution,
@@ -92,27 +93,30 @@ Deno.test("doesn't resolve the same uri twice", async () => {
   assertEquals(new Set(duplicateUris), new Set());
 });
 
-Deno.test("can validate a post record", async () => {
-  const NSID_STR = "app.bsky.feed.post";
-  const registry = bootstrap(NodeRegistry);
-  const resolutions = [];
-  for await (
-    const resolution of registry.resolve([
-      registry.get(NSID.parse(NSID_STR)),
-    ])
-  ) {
-    assertSuccessfullResolution(resolution);
-    resolutions.push(resolution);
-  }
+Deno.test,
+  skip("can validate a post record", async () => {
+    const NSID_STR = "app.bsky.feed.post";
+    const registry = bootstrap(NodeRegistry);
+    const resolutions = [];
+    for await (
+      const resolution of registry.resolve([
+        registry.get(NSID.parse(NSID_STR)),
+      ])
+    ) {
+      assertSuccessfullResolution(resolution);
+      resolutions.push(resolution);
+    }
 
-  const lexicons = new Lexicons(resolutions.map((r) => r.doc));
+    const lexicons = new Lexicons(resolutions.map((r) => r.doc));
 
-  const exampleRecords = [
-    '{"uri":"at://did:plc:2xau7wbgdq4phuou2ypwuen7/app.bsky.feed.post/3ljrsfbuuyk2g","cid":"bafyreigjrpyhhk5fibcajhhtkcefmxptntgx35t5zngvsxwwvu56hzioaq","value":{"text":"To update a globally installed CLI in deno you pass -fr\\n\\nfr fr","$type":"app.bsky.feed.post","langs":["en"],"createdAt":"2025-03-07T10:28:47.874Z"}}',
-    '{"uri":"at://did:plc:yosojsta3nm5qiod5zqixzur/app.bsky.feed.post/3ljpreuc6fs2c","cid":"bafyreihyt2ckoxoyusimrexuxatuwazct7uiskukhspxic756inddofipy","value":{"text":"DOGE “seemed unsure” of what USAID programs they cut and is now attempting to reverse some of the cuts, says @propublica.org reporter Brett Murphy. “This is the opposite of a careful review.\\"","$type":"app.bsky.feed.post","embed":{"$type":"app.bsky.embed.video","video":{"$type":"blob","ref":{"$link":"bafkreidowys6ntilo4wslx23jishxiqfrwywqmxxer4sy2r6y5orwuosn4"},"mimeType":"video/mp4","size":8408607},"aspectRatio":{"width":1280,"height":720}},"langs":["en"],"facets":[{"$type":"app.bsky.richtext.facet","index":{"byteEnd":128,"byteStart":113},"features":[{"did":"did:plc:k4jt6heuiamymgi46yeuxtpt","$type":"app.bsky.richtext.facet#mention"}]}],"createdAt":"2025-03-06T15:05:20.412Z"}}',
-  ].map((s) => JSON.parse(s));
+    const exampleRecords = [
+      '{"uri":"at://did:plc:2xau7wbgdq4phuou2ypwuen7/app.bsky.feed.post/3ljrsfbuuyk2g","cid":"bafyreigjrpyhhk5fibcajhhtkcefmxptntgx35t5zngvsxwwvu56hzioaq","value":{"text":"To update a globally installed CLI in deno you pass -fr\\n\\nfr fr","$type":"app.bsky.feed.post","langs":["en"],"createdAt":"2025-03-07T10:28:47.874Z"}}',
+      '{"uri":"at://did:plc:yosojsta3nm5qiod5zqixzur/app.bsky.feed.post/3ljpreuc6fs2c","cid":"bafyreihyt2ckoxoyusimrexuxatuwazct7uiskukhspxic756inddofipy","value":{"text":"DOGE “seemed unsure” of what USAID programs they cut and is now attempting to reverse some of the cuts, says @propublica.org reporter Brett Murphy. “This is the opposite of a careful review.\\"","$type":"app.bsky.feed.post","embed":{"$type":"app.bsky.embed.video","video":{"$type":"blob","ref":{"$link":"bafkreidowys6ntilo4wslx23jishxiqfrwywqmxxer4sy2r6y5orwuosn4"},"mimeType":"video/mp4","size":8408607},"aspectRatio":{"width":1280,"height":720}},"langs":["en"],"facets":[{"$type":"app.bsky.richtext.facet","index":{"byteEnd":128,"byteStart":113},"features":[{"did":"did:plc:k4jt6heuiamymgi46yeuxtpt","$type":"app.bsky.richtext.facet#mention"}]}],"createdAt":"2025-03-06T15:05:20.412Z"}}',
+    ].map((s) => JSON.parse(s).value);
 
-  for (const exampleRecord of exampleRecords) {
-    lexicons.validate(NSID_STR, exampleRecord);
-  }
-});
+    for (const exampleRecord of exampleRecords) {
+      const result = lexicons.validate(NSID_STR, exampleRecord);
+      // @ts-expect-error result.error isn't narrowed
+      assertEquals(result.success, true, `Failed to validate: ${result.error}`);
+    }
+  });
